819c46803a5f56c081a30a95bba971c870f59fea
Saddam Azy <myxzlpltk@gmail.com>
2020-12-07 21:02:41 +0700

    remove property next on class State

--- src/State.java
+++ src/State.java
@@ -6,7 +6,6 @@ public class State {
 
     private String file;
     private BufferedImage image;
-    private State next;
     private int color;
 
     public State(String file, int color) throws IOException {
